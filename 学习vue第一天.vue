d5690f5cb30f36eadead94a88bd66d926df8e7d8
黄海峰 <18363827383@163.com>
2020-04-12 22:37:04 +0800

    *提交 qa!

---
+++ 学习vue第一天.vue
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	    <script src="vue.js" type="text/javascript" charset="utf-8"></script>
+		<!-- <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script> -->
+	</head>
+	<body>
+		<div id="app">
+          {{message}}  {{NAME}}  {{AGE}}
+		</div>
+		<script type="text/javascript">
+			var app=new Vue({   /* VUE对象里包含element和data数据，el对应着div标签的id，data对应着message数据，初始化一个数据值为HELLO VUES*/
+				el:'#app',
+				data:{
+					message:'HELLO VUE!',
+					NAME:'Dm',
+					AGE:'SAD'
+				}
+				
+			});
+		</script>
+	</body>
+</html>
